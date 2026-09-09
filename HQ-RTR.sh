65a3d577ef88c394773b86a70509cc6ac5ac5b07
19zammik86-source <19zammik86@gmail.com>
2026-09-09 14:07:21 +0700

    Update HQ-RTR.sh

--- HQ-RTR.sh
+++ HQ-RTR.sh
@@ -112,7 +112,7 @@ echo "Настройка OSPF завершена!"
 
 #ставим NAT 
 apt-get install iptables -y
-iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE 
+iptables -t nat -A POSTROUTING -o enp7s1 -j MASQUERADE 
 #iptables -t nat -A PREROUTING -p tcp -d 192.168.100.1 --dport 2027 -j DNAT --to-destination 192.168.100.2:2027
 iptables-save >> /etc/sysconfig/iptables
 systemctl enable --now iptables
