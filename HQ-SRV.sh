238b674724dc3087feb751ae0f66a7087874f0b7
19zammik86-source <19zammik86@gmail.com>
2026-09-14 09:36:24 +0700

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+hostnamectl set-hostname br-rtr.au-team.irpo
+
+apt-get update && apt-get install -y chrony tzdata
+
+cat <<EOF > /etc/chrony.conf
+        pool 172.16.1.1 iburst
+        driftfile /var/lib/chrony/drift
+        makestep 1.0 3
+        rtcsync
+        ntsdumpdir /var/lib/chrony
+        logdir /var/log/chrony
+        EOF
+        
+# Настрока часового пояса
+timedatectl set-timezone Asia/Krasnoyarsk
+
+
+systemctl enable --now chronyd
+        systemctl restart chronyd
 
 # Установка wget
 apt-get update && apt-get install wget
