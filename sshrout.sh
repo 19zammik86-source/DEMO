4f0899bbf98c9edbc10012b0444037275128d6c2
19zammik86-source <19zammik86@gmail.com>
2026-09-15 09:54:04 +0700

    Create sshrout.sh

---
+++ sshrout.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 1. Создание пользователя net_admin (ЛИБО ДРУГОГО ПОЛЬЗОВАТЕЛЯ, ПРИ СМЕНЕ ПОМЕНЯТЬ В ЭТОМ ФАЙЛЕ ИМЯ И Т.Д)
+useradd -m net_admin
+
+# 2. Установка пароля P@$$word (без подтверждения)
+echo "net_admin:P@ssw0rd" | chpasswd
+
+# 3. Добавление в группу wheel
+apt-get install -y shadow-groups
+gpasswd -a net_admin wheel
+
+# 4. Настройка sudo без пароля
+echo "net_admin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# 5. Настройка SSH (порт 2027 и запрет root-логина)
+sed -i 's/#Port 22/Port 2027/' /etc/openssh/sshd_config
+sed -i 's/#PermitRootLogin without-password/PermitRootLogin no/' /etc/openssh/sshd_config
+
+# 6. Перезапуск SSH
+systemctl restart sshd
+systemctl enable --now sshd
+echo "Готово! Пользователь net_admin создан, SSH настроен на порт 2027."
