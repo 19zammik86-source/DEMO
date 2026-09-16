1021f52a909e077d3d687e8e142b5fc8a4ad9d6e
19zammik86-source <19zammik86@gmail.com>
2026-09-16 15:36:35 +0700

    Update fail2.sh

--- fail2.sh
+++ fail2.sh
@@ -3,7 +3,7 @@ apt-get install -y fail2ban python3-module-systemd
 sed -i 's/before = paths-altlinux.conf/before = paths-altlinux-systemd.conf/' /etc/fail2ban/jail.conf
 sed -i '/^\[sshd\]/a\enabled = true' /etc/fail2ban/jail.conf
 sed -i '/^\[sshd\]/,/^\[/{s/^port.*/port    = 2027/}' /etc/fail2ban/jail.conf
-sed -i '/^\[sshd\]/,/^\[/s/port.*2026/&\nmaxretry = 3/' /etc/fail2ban/jail.conf
+sed -i '/^\[sshd\]/,/^\[/s/port.*2027/&\nmaxretry = 3/' /etc/fail2ban/jail.conf
 sed -i '/^\[sshd\]/,/^\[/s/maxretry.*3/&\nfindtime = 10m/' /etc/fail2ban/jail.conf
 sed -i '/^\[sshd\]/,/^\[/s/findtime.*10m/&\nbantime = 1m/' /etc/fail2ban/jail.conf
 systemctl enable --now fail2ban
