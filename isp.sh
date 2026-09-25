3ad52120e8588650ab44ef001254918c98f4532d
19zammik86-source <19zammik86@gmail.com>
2026-09-25 11:53:55 +0700

    Update isp.sh

--- isp.sh
+++ isp.sh
@@ -66,15 +66,3 @@ apt-get update
 
 exec bash
 
-cat <<EOF > /etc/chrony.conf
-        pool pool.ntp.org iburst
-        local stratum 5
-        allow 0/0
-        driftfile /var/lib/chrony/drift
-        makestep 1.0 3
-        ntsdumpdir /var/lib/chrony
-        logdir /var/log/chrony
-        EOF
-
-        systemctl enable --now chronyd
-        systemctl restart chronyd
